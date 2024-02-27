9beec9cb6339b412f4674bb5d89af43fd33ae900
iitzrohan <t.rohan1903@gmail.com>
2024-02-28 05:11:10 +0530

    Add admin page and handle job requests.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,16 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Metadata } from "next";
+import AdminNavbar from "./AdminNavbar";
 
 export const metadata: Metadata = {
   title: "Admin",
 };
 
-export default function layout({ children }: { children: React.ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <AdminNavbar />
+      {children}
+    </ClerkProvider>
+  );
 }
