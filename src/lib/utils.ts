ef8dbde242a5b17e2a33031af32cafd905e27d17
iitzrohan <t.rohan1903@gmail.com>
2024-02-28 04:19:05 +0530

    Add createJobPosting function and use server actions for job posting.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,12 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx";
 import { formatDistanceToNowStrict } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatCurrency(amount: number) {
+export function formatMoney(amount: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -15,4 +15,11 @@ export function formatCurrency(amount: number) {
 
 export function relativeDate(from: Date) {
   return formatDistanceToNowStrict(from, { addSuffix: true });
+}
+
+export function toSlug(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "");
 }
